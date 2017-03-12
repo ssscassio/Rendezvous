6d0d379d91fae5da4d12045c7251809c0ef2121f
Cássio Santos <sss.cassio@gmail.com>
2017-03-12 12:50:21 -0300

    Update em código CUDA

--- Projeto CUDA/rendezvous.cu
+++ Projeto CUDA/rendezvous.cu
@@ -204,3 +204,143 @@ double __device__ brute_J(int N, double x0, double y0, double z0, double xl0, do
     result = sum;
     return result;
 }
+
+void __device__ brute_all(double *A, int N, double x0, double y0, double z0, double xl0, double yl0, double zl0, double Y, int X, double w, double vex, double vey, double vez){
+
+    double a, B, D, e, G, H, I;    
+    //Calculando valores de A, B, C, D, E, F, G, H, I e J
+    a = brute_A(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 0, vex, vey,vez); 
+    B = brute_B(N,x0,y0,z0,xl0,yl0,zl0,  Y, X, w, 0, vex, vey,vez);
+    D = brute_D(N,x0,y0,z0,xl0,yl0,zl0,Y, X, w, 0, vex, vey,vez);
+    e = brute_E(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 0, vex, vey,vez);
+    G = brute_G(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 0, vex, vey,vez);
+    H = brute_H(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 0, vex, vey,vez);
+    I = brute_I(N,x0,y0,z0,xl0,yl0,zl0,  Y, X, w, 0, vex, vey,vez);
+
+     //Calculando inicialmente a soma (A1² + A3² + A5²)
+    A[1]  = 2*a*w + e - Y*brute_F(N,x0,y0,z0, xl0,yl0, zl0, Y, X, w, 1, vex, vey,vez);
+    A[3]  = B*w - Y*brute_C(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 1, vex, vey,vez);
+    A[5]  = I*w +Y*brute_J(N,x0,y0,z0,xl0, yl0, zl0, Y, X, w, 1, vex, vey,vez);
+
+     //Calculando inicialmente a soma (A2² + A4² + A6²)
+    A[2]  = G - 2*B + brute_F(N,x0,y0,z0,xl0,yl0, zl0, Y, X, w, 0, vex, vey,vez);
+    A[4]  = a +D + brute_C(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 0, vex, vey,vez);
+    A[6]  = H - brute_J(N,x0,y0,z0,xl0,yl0,zl0,  Y, X, w, 0, vex, vey,vez);
+
+     //Calculando inicialmente a soma (A7² + A9² + A11²)
+    A[7]  = 2*B*w*w + Y*Y*brute_F(N,x0,y0,z0, xl0, yl0, zl0, Y, X, w, 2, vex, vey,vez);
+    A[9]  = Y*Y*brute_C(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 2, vex, vey,vez) - a*w*w;
+    A[11] = -w*w*H - Y*Y*brute_J(N,x0,y0,z0,xl0,yl0,zl0, Y, X, w, 2, vex, vey,vez);
+
+     //Calculando inicialmente a soma (A8² + A10² + A12²) A8=A1 // A10 = A3 // A12 = A5
+    A[8]  = A[1];
+    A[10] = A[3];
+    A[12] = A[5];
+
+    __syncthreads();
+}
+
+double __device__ calcularDiferenca (int N, double x0, double y0, double z0, double xl0, double yl0, double zl0, double gama, double chi, double w, double vex, double vey, double vez){
+    double A[13];
+    double a,b,c,d;
+    double a1,a2,a3,a4,a5,a6;
+    double result;
+
+    brute_all(A, 10, x0, y0, z0, xl0, yl0, zl0, gama, chi, w, vex, vey, vez);
+    a1 = 1/(A[1]*A[1]+A[3]*A[3]+A[5]*A[5]);
+    a2 = A[2]*A[2]+A[4]*A[4]+A[6]*A[6];
+    a3 = 1/(A[7]*A[7]+A[9]*A[9]+A[11]*A[11]);
+    a4 = A[8]*A[8]+A[10]*A[10]+A[12]*A[12];
+    a5 = A[1]*A[2] + A[3]*A[4] + A[5]*A[6];
+    a6 = A[7]*A[8] + A[9]*A[10] + A[11]*A[12];
+
+    a = a5*a1;
+    b = a1*a2;
+    c = a6*a3;
+    d = a3*a4;
+    result = pow(b-d,2)-4*(a-c)*(b*c-a*d);
+
+    return result;
+}
+
+void __global__ calcularRendezvousDevice(double d_x0,double d_y0,double d_z0,double d_xl0,double d_yl0,double d_zl0,double d_w){
+
+  double Y = blockIdx.x; //Y(Gama) recebe o valor x atual do bloco;
+  double X = threadIdx.x; //X(Chi) recebe o valor y atual do thread;
+  double ve = blockIdx.y; //ve||vex(Velocidade de exaustão) recebe o valor z atual do thread;
+
+
+}
+
+void calcularRendezvous(double x0, double y0, double z0, double xl0, double yl0, double zl0, double w) {
+    double vex,vey,vez; //Variaveis Iteraveis //Componentes das Velocidades de exaustao
+    double chi; //Variavel Iteravel
+    double gama; //Variavel Iteravel
+
+    int tx = 16; //Número de iterações em Gama (de -14 a 2 com passo 1)
+    int ty = 99; //Número de iterações em Chi (de 1 a 100 com passo 1)
+    int tz = 99; //Número de iterações em ve (de 0.1 a 10 com passo 0.1)
+    dim3 numBlocks(tx,tz);
+    size_t size = sizeof(double);
+    dim3 threadsPerBlock(ty);
+
+    //Alocando a memória das variáveis no Device
+    cudaMalloc((void**)&d_x0, size);
+    cudaMalloc((void**)&d_y0, size);
+    cudaMalloc((void**)&d_z0, size);
+    cudaMalloc((void**)&d_xl0, size);
+    cudaMalloc((void**)&d_yl0, size);
+    cudaMalloc((void**)&d_zl0, size);
+    cudaMalloc((void**)&d_w, size);
+
+    //Copiando as variáveis do host para o Device
+    cudaMemcpy(&d_x0, &x0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_y0, &y0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_z0, &z0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_xl0, &xl0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_yl0, &yl0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_zl0, &zl0, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(&d_w, &w, size, cudaMemcpyHostToDevice);
+
+    calcularRendezvousDevice<<<numBlocks, threadsPerBlock>>>(d_x0,d_y0,d_z0,d_xl0,d_yl0,d_zl0,d_w);
+
+}
+
+int main(int argc, char **argv){
+    //Declaração das variaveis
+    double x0,y0,z0; //Componentes das posições relativas
+    double xl0,yl0,zl0; //Componentes das velocidades relativas
+    double w; //Raio (Constante)
+    double raio; //raio (Constante)
+    double r0; //Altitude <-----Verificar ----->
+    
+    //Variaveis inutilizadas (Por enquanto)
+    double tempo, alpha, beta, vi, xf, yf, zf, rf, dxf, dyf, vf;
+    //Variaveis para modo Debug
+    double gama, chi, ve, vex, vey, vez;
+
+    if(argc == 1){
+        printf("Passe o nome dos arquivos de input como parâmetro\n");
+        return 1;
+    }
+
+    for(int i = 1; i < argc; i++){ //Tentativa de leitura de cada um dos arquivos passados como parâmetro
+        /*Leitura do Arquivo*/
+        char * nomeDoArquivo = argv[i];
+        FILE *file;
+        file = fopen(nomeDoArquivo, "r"); 
+        if (file == NULL) { //Verifica se o caminho existe
+            break;
+        } else {
+            while((fscanf(file,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", &tempo, &alpha, &beta, &x0, &y0, &z0, &r0, &xl0, &yl0, &zl0, &vi, &xf, &yf, &zf, &rf, &dxf, &dyf, &zf, &vf)) != EOF){ //Enquanto não for o fim do arquivo
+                // Tempo Alpha Beta X0 y0 z0 r0 xl0 yl0 zl0 |Vi| xf yf zf rf dxf dyf dzf |Vf|
+                // 456.000000 104 89 -0.725655 2.910444 0.052357 3.000000 0.005108 -0.006719 -0.000104 0.008441 0.000000 0.000000 0.000000 0.000000 -0.001749 -0.005737 -0.000121 0.005999
+                raio = EARTH_RADIUS + r0;
+                w = sqrt(MI/(raio*raio*raio));
+                calcularRendezvous(x0,y0,z0,xl0,yl0,zl0,w);
+
+            }
+        }
+    }
+
+}
